3fc1d9b5df3133edc4f055570f03fb2cdb6cd741
harshitmalik03 <malikharshit.333@gmail.com>
2022-09-23 19:54:10 +0530

    added tailwind

---
+++ src/Comp.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Table, TableContainer } from '@material-ui/core';
+
+function Comp() {
+  return (
+    <div className="App">
+        
+      <div className=''>Hello World</div>
+    </div>
+  );
+}
+
+export default Comp;
